00f6e65f7d23c6e9ed98b3a8f5c153f3f55a30e3
Stefano Ghirlanda <drghirlanda@gmail.com>
2023-04-12 17:41:39 -0400

    added accept-reject functionality

--- org-change.el
+++ org-change.el
@@ -1,4 +1,4 @@
-;;; org-change.el --- Track changes in org-mode files
+;;; org-change.el --- Track changes in org-mode files -*- lexical-binding: t;
 
 ;; Copyright (C) 2023 Stefano Ghirlanda
 
@@ -22,49 +22,29 @@
 
 ;;; Commentary:
 
-;; This package provides a syntax for tracking changes in org-mode
-;; files, using org links and the changes package for LaTeX export.
+;; This package provides a minor mode for tracking changes in org-mode
+;; files, by defining a new type of link, the change: link. Using the
+;; functions org-change-add, org-change-delete, and
+;; org-change-replace, you can mark text as constitution an addiion,
+;; deletion, or replacement to the text. These functions are bound by
+;; default to C-` a, C-` d, and C-` r. Functions org-change-accept and
+;; org-change-reject can be used to replace the change: link with the
+;; new and old text, respectively. These are bound to C-` o and C-` x.
+;;
+;; To change key bindings and other settings, run M-x customize-group
+;; RET org-change. When exporting to LaTeX, changes are rendered using
+;; the "changes" package. See the package URL for more documentation.
 
 ;;; Code:
 
-;; 
-(defgroup org-change nil
-  "Customization options for org-mode change syntax."
-  :group 'org)
+;; Code 
 
-;;
-(defcustom org-change-delete-key (kbd "C-c d")
-  "Keybinding for deleting the active region (hiding it into a
-change link and showing **DELETED** in its place)."
-  :type 'key-sequence
-  :group 'org-change)
+(setq org-change--deleted-marker "((DELETED))")
 
-;;
-(defcustom org-change-replace-key (kbd "C-c r")
-  "Keybinding for replacing the active region (hiding it into a
-change link and placing the point to insert replacement text)."
-  :type 'key-sequence
-  :group 'org-change)
-
-;;
-(defcustom org-change-add-key (kbd "C-c a")
-  "Keybinding for tagging the active region as added text."
-  :type 'key-sequence
-  :group 'org-change)
-
-(define-minor-mode org-change-mode
-  "Minor mode for tracking changes in org-mode files."
-  :lighter " Chg"
-  :group 'org-change
-  :keymap (let ((map (make-sparse-keymap)))
-            (define-key map org-change-add-key 'org-change-add)
-            (define-key map org-change-delete-key 'org-change-delete)
-            (define-key map org-change-replace-key 'org-change-replace)
-            map))
-
-;;
 (defun org-change-replace ()
-  ""
+  "Mark the active region as old text that is being replaced by new
+text. You will be prompted for the new text, and the old text
+will be hidden in a change: link."
   (interactive "")
   (if (not (use-region-p))
       (message "Select text to be replaced")
@@ -75,21 +55,25 @@ change link and placing the point to insert replacement text)."
 		      (read-string "New text: ")))
       (delete-region beg end))))
 
-;;
 (defun org-change-delete ()
-  ""
+  "Mark the active region as text that is being deleted. The text
+will be hidden in a change: link and a DELETED marker will be
+shown in its place."
   (interactive "")
   (if (not (use-region-p))
       (message "Select text to be deleted")
     (let ((beg (region-beginning))
 	  (end (region-end)))
-      (insert (format "[[change:%s][**DELETED**]]" 
-		      (buffer-substring-no-properties beg end)))
+      (insert (format "[[change:%s][%s]]" 
+		      (buffer-substring-no-properties beg end)
+		      org-change--deleted-marker))
       (delete-region beg end))))
 
 ;;
 (defun org-change-add ()
-  "If the region is active, mark it as an addition. If the region is not active, prompt the user for text to add. The added text is embedded in a change link as [[change:][added text]]."
+  "Mark the active region as text that is being added. If the
+region is not active, you will be prompted for text to add. The
+added text is shown in a change: link."
   (interactive "")
   (let	((beg (if (use-region-p) (region-beginning) (point)))
 	 (end (if (use-region-p) (region-end) (point))))
@@ -99,19 +83,40 @@ change link and placing the point to insert replacement text)."
 		      (read-string "New text: "))))
     (delete-region beg end)))
 
+
+(defun org-change--accept-or-reject (accept)
+  ""
+  (setq link-position (org-in-regexp "\\[\\[change:\\(.*\\)\\]\\[\\(.*\\)\\]\\]" 10))
+  (if link-position
+      (let ((old-text (match-string-no-properties 1))
+	    ;; to get new-text we also discard comments, if present: 
+	    (new-text (replace-regexp-in-string "\\*\\*.+\\*\\*$" "" (match-string-no-properties 2)))
+	    (beg (car link-position))
+	    (end (cdr link-position)))
+	(delete-region beg end)
+	(if accept
+	    (if (not (equal new-text org-change--deleted-marker))
+		(insert new-text))
+	  (insert old-text)))
+    (message "There is no change: link here")))
+
+(defun org-change-accept ()
+  "Accept the change at point, replacing the change: link with the
+new text and erasing the old text or **DELETE** marker."
+  (interactive "")
+  (org-change--accept-or-reject t))
+
+(defun org-change-reject ()
+  "Reject the change at point, replacing the change: link with the
+old text and erasing the new text or **DELETE** marker."
+  (interactive "")
+  (org-change--accept-or-reject nil))
+
+;; Tell org-mode about change: links 
+
 (defface org-change-link-face
-  '((t (:foreground "blue" :underline nil)))
+  '((t (:foreground "orange" :underline nil)))
   "Face for org-change links.")
-
-(defgroup org-change nil
-  "Customization options for org-change."
-  :group 'org
-  :prefix "org-change-")
-
-(defcustom org-change-face 'org-change-link-face
-  "Face for org-change links."
-  :type 'face
-  :group 'org-change)
 
 (org-link-set-parameters "change"
                          :follow #'org-change-open-link
@@ -132,19 +137,69 @@ change link and placing the point to insert replacement text)."
   (if (string-match "\\(.*\\)\\*\\*\\(.+\\)\\*\\*$" new-text-raw)
       (progn
 	(setq new-text (match-string 1 new-text-raw))
-	(setq comment  (format "[comment=%s]" (match-string 2 new-text-raw))))
+	(setq comment  (match-string 2 new-text-raw)))
     (progn
       (setq new-text new-text-raw)
       (setq comment "")))
   (cond ((org-export-derived-backend-p backend 'latex)
-	 (cond ((equal old-text "")
-		(format "\\added%s{%s}" comment new-text))
-	       ((equal new-text "**DELETED**")
-		(format "\\deleted%s{%s}" comment old-text))
-	       (t
-		(format "\\replaced%s{%s}{%s}" comment new-text old-text))))
+	 (progn
+	   (if (not (equal comment ""))
+	       (setq comment (format "[comment=%s]" comment)))
+	   (cond ((equal old-text "")
+		  (format "\\added%s{%s}" comment new-text))
+		 ((equal new-text org-change--deleted-marker)
+		  (format "\\deleted%s{%s}" comment old-text))
+		 (t
+		  (format "\\replaced%s{%s}{%s}" comment new-text old-text)))))
 	(t
 	 (error (format "Change links not supported in %s export" backend)))))
+
+;; Customizations and minor mode definitions
+
+(defgroup org-change nil
+  "Customization options for org-change."
+  :group 'org)
+
+(defcustom org-change-delete-key (kbd "C-` d")
+  "Keybinding for org-change-delete."
+  :type 'key-sequence
+  :group 'org-change)
+
+(defcustom org-change-replace-key (kbd "C-` r")
+  "Keybinding for org-change-replace."
+  :type 'key-sequence
+  :group 'org-change)
+
+(defcustom org-change-add-key (kbd "C-` a")
+  "Keybinding for org-change-add."
+  :type 'key-sequence
+  :group 'org-change)
+
+(defcustom org-change-accept-key (kbd "C-` k")
+  "Keybinding for org-change-accept."
+  :type 'key-sequence
+  :group 'org-change)
+
+(defcustom org-change-accept-key (kbd "C-` x")
+  "Keybinding for org-change-reject."
+  :type 'key-sequence
+  :group 'org-change)
+
+(defcustom org-change-face 'org-change-link-face
+  "Face for org-change links."
+  :type 'face
+  :group 'org-change)
+
+(define-minor-mode org-change-mode
+  "Minor mode for tracking changes in org-mode files."
+  :lighter " Chg"
+  :group 'org-change
+  :keymap (let ((map (make-sparse-keymap)))
+            (define-key map org-change-add-key #'org-change-add)
+            (define-key map org-change-delete-key #'org-change-delete)
+            (define-key map org-change-replace-key #'org-change-replace)
+            (define-key map org-change-accept-key #'org-change-accept)
+            map))
 
 (provide 'org-change)
 
